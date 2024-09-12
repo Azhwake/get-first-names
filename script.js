cd8c6f3169c1e941bc98105c312a721dbe454eb6
Azhwake <azhwaq2002@gmail.com>
2024-09-12 14:43:05 +0300

    done

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 // Good Luck 💪🏾
+function getFirstNames(nestedArray) {
+    let firstNames = [];
+    for (let i = 0; i < nestedArray.length; i++) {
+        firstNames.push(nestedArray[i][0]);
+    }
+    return firstNames;
+}
+
+// Test case
+console.log(getFirstNames([["Ahmed", "Aisha", "Amina"], ["Jaamac", "Khadijah", "Layla"], ["Mohamed", "Ali"]]));
+// ➞ ["Ahmed", "Jaamac", "Mohamed"]
